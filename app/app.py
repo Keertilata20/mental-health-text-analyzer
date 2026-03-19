c3c0fcfc9f6e7553c311b98c4cd84669fcbacac0
Keerti Lata Choudhury <keerti.lc05@gmail.com>
2026-03-19 19:46:27 +0530

    added AI responses to chatbot

--- app/app.py
+++ app/app.py
@@ -3,6 +3,14 @@ import joblib
 import random
 import time
 import pandas as pd
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 # -----------------------------
 # PAGE CONFIG
